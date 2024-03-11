ac5654124213b918f8cea3828b53858ee37f6243
German <germants100@gmail.com>
2024-03-10 20:32:34 -0600

    creating endpoints and views

--- back/API/API/urls.py
+++ back/API/API/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from menuManage.views import login, register, profile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login),
+    path('register/', register),
+    path('profile/', profile),
 ]
---
+++ back/API/menuManage/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['POST'])
+def login (request):
+    return Response({'login'})
+
+@api_view(['POST'])
+def register(request):
+    return Response({})
+
+@api_view(['POST'])
+def profile(request):
+    return Response({})
