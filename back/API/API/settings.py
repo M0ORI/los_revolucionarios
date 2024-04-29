cb927bba79721e059e642ce6efd59d52e8e26542
eddy <mauricio.gonsales.23@outlook.com>
2024-04-28 20:54:39 -0600

    database update

--- back/API/API/settings.py
+++ back/API/API/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'API.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} """
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "revolucionarios",
+        "USER": "admin",
+        "PASSWORD": "4kArfa05qH4tuywVWQy768fCibOKJgtY",
+        "HOST": "dpg-congfli1hbls73fid0s0-a.oregon-postgres.render.com",
+        "PORT": "5432",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
