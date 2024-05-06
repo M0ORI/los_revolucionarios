d85e37feacb5170a487f56baadd3037ee2614629
eddy <mauricio.gonsales.23@outlook.com>
2024-05-05 21:25:34 -0600

    errors update: navbar and footer

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,10 @@
 import './app.css'
-import InvNavrbar from './components/InvNavbar/InvNavrbar'
+
 import Navbar from './components/Navbar/Navbar'
+import Carrusel from './components/Carrusel/Carrusel'
+import Body from './components/Body/Body'
+import Footer from './components/Footer/Footer'
+import InvNavrbar from './components/InvNavbar/InvNavrbar'
 
 
 function App() {
@@ -8,10 +12,9 @@ function App() {
   return (
     <>
       <Navbar/>
-      {/* <Navbar/>
       <Carrusel/>
       <Body/>
-      <Footer/> */}
+      <Footer/> 
       <InvNavrbar/>
     </>
   )
