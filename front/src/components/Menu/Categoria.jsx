bd113d36d93966d07057b5d2a3857cccecee4812
German <germants100@gmail.com>
2024-05-16 21:36:21 -0600

    Adding comments

--- front/src/components/Menu/Categoria.jsx
+++ front/src/components/Menu/Categoria.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import styles from './Categoria.module.css'
-import Platillo from './Platillo'
+import Platillo from './Platillo' /*Importamos el componente de platillo */
 
-const Categoria = ({nombre, platillos}) => {
+const Categoria = ({nombre, platillos}) => { /*Se reciben los atributos necesarios para crear la categoria */
   return (
     <section className={styles.contenedor}>
         <h1 className={styles.tituloCategoria}> {nombre} </h1>
         <div className={styles.contenedorPlatillos}>
           {
-            platillos.map((platillo, id) => {
+            platillos.map((platillo, id) => { /*Se hace uso de la funcion map para extraer los datos del objeto platillos, y asi hacer de cada uno un componente */
               return (
                 <Platillo nombre={platillo.nombre} img={platillo.img} descripcion={platillo.descripcion} key={id} />
               )
--- front/src/components/Menu/Menu.jsx
+++ front/src/components/Menu/Menu.jsx
@@ -4,9 +4,9 @@ import Categoria from './Categoria'
 import InvNavrbar from '../InvNavbar/InvNavrbar'
 import Contacto from '../Contacto/Contacto'
 
-import menuData from '../../data/menu.json'
+import menuData from '../../data/menu.json' /*Importamos el archivo JSON que contiene la infomacion de las categorias con sus respectivos paltillos*/
 
-import './Menu.css'
+import './Menu.css' /*Importamos el estilo  */
 
 const Menu = () => {
   return (
@@ -15,16 +15,16 @@ const Menu = () => {
       <div className='divisor'></div>
       <main>
         {
-          menuData.map((categoria, id) => {
+          menuData.map((categoria, id) => { /*Usamos la funcion map para renderizar cada categoria con sus respctivos platillos en componentes, haciendo uso de la informacion contenida */
             return (
               <Categoria nombre={categoria.categoria} platillos={categoria.platillos} key={id} />
             )
           })
         }
-      <div className='contenedorBoton'>
+      <div className='contenedorBoton'> {/*Div usado para dividir la pagina */}
         <p>Descarga nuestro menu completo</p>
         <a href="/public/MENU.pdf" download>
-          <button>Menu Completo</button>
+          <button>Menu Completo</button> {/*Boton para descargar el menu*/}
         </a>
       </div>
       <div className='divisor' ></div>
