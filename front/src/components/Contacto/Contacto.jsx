f8f31633f6a89eb7677d7f1fee77fea3d4500f6f
German <germants100@gmail.com>
2024-05-21 16:03:22 -0600

    Adding responsive and andother icon to contact component

--- front/src/components/Contacto/Contacto.jsx
+++ front/src/components/Contacto/Contacto.jsx
@@ -2,20 +2,27 @@ import React from 'react'
 import styles from './Contacto.module.css'
 import icon1 from '../../../public/images/Icons/phone-call.png'
 import icon2 from '../../../public/images/Icons/direccion.png'
+import icon3 from '../../../public/images/Icons/whatsapp.png'
 const Contacto = () => {
   return (
     <div className={styles.main}>
       <ul className={styles.contenedor}>
-        <li className={styles.item}>
+      <li className={styles.item}>
             <a href="">
-                <h2 className={styles.texto}>Llamanos</h2>
-                <img src={icon1} alt="" />
+                <h2 className={styles.texto}>Ubicanos</h2>
+                <img src={icon2} alt="" />
             </a>
         </li>
         <li className={styles.item}>
             <a href="">
-                <h2 className={styles.texto}>Visitanos</h2>
-                <img src={icon2} alt="" />
+                <h2 className={styles.texto}>WhatsApp</h2>
+                <img src={icon3} alt="" />
+            </a>
+        </li>
+        <li className={styles.item}>
+            <a href="">
+                <h2 className={styles.texto}>Llamanos</h2>
+                <img src={icon1} alt="" />
             </a>
         </li>
       </ul>
