cb0b2c496679f8a511d089dd64c19725eb22ef4a
eddy <mauricio.gonsales.23@outlook.com>
2024-05-23 23:38:12 -0600

    app update

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -5,9 +5,24 @@ import Carrusel from './components/Carrusel/Carrusel'
 import Body from './components/Body/Body'
 import Footer from './components/Footer/Footer'
 import InvNavrbar from './components/InvNavbar/InvNavrbar'
+import { useEffect } from 'react'
 
 
 function App() {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/menu/getPresentaciones/');
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); 
 
   return (
     <>
